5da4e816c3a5060ecf08146171d687fc345e3f14
George Lifchits <george.lifchits@gmail.com>
2013-05-14 23:35:07 -0400

    contact email system; minor changes

--- web/app.py
+++ web/app.py
@@ -3,7 +3,7 @@ Created on 2013-04-30
 
 @author: glifchits
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 def create_app(config):
     app = Flask(__name__)
@@ -17,8 +17,34 @@ def create_app(config):
     def resume():
         return render_template('resume.html', title_modifier=" - Resume")
 
-    @app.route('/contact/')
+    @app.route('/contact/', methods=['GET', 'POST'])
     def contact():
-        return render_template('contact.html', title_modifier=" - Contact")
+        if request.method == "POST":
+            return send_email(request)
+        else:
+            return render_template('contact.html', title_modifier=" - Contact")
+
+    def send_email(response):
+        try:
+            the_sender = response.form['sender']
+            the_subject = response.form['subject']
+            body_text = response.form['message']
+        
+            if the_subject == "": the_subject = "Contact: No subject"
+            if body_text == "": body_text = "No body text entered"
+            
+            from google.appengine.api import mail
+            
+            message = mail.EmailMessage(sender=the_sender, subject=the_subject)
+            message.to = "George Lifchits <george.lifchits@gmail.com>"
+            message.body = body_text
+    
+            message.send()
+            return send_success(True)
+        except:
+            return send_success(False)
+
+    def send_success(response = True):
+        return render_template('contact.html', send_success = response)
 
     return app
